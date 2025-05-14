df8f19582a091f5915c5e64fe5c5903b6c4e2c23
abdlaziz07 <abdl.azzz0702@gmail.com>
2025-05-14 13:08:41 +0700

    Update call_center_dashboard.py

--- call_center_dashboard.py
+++ call_center_dashboard.py
@@ -41,26 +41,34 @@ top_reasons = filtered_df['Reason'].value_counts().nlargest(10)
 fig2 = px.bar(x=top_reasons.values, y=top_reasons.index, orientation='h', labels={'x':'Jumlah Panggilan', 'y':'Alasan'}, color=top_reasons.index)
 st.plotly_chart(fig2, use_container_width=True)
 
-# 3. Distribusi Sentimen
+# 3. Kota dengan Jumlah Panggilan Terbanyak
+st.subheader("🏙️ Top 10 Kota dengan Jumlah Panggilan Terbanyak")
+st.markdown("""Grafik ini menampilkan kota-kota dengan volume panggilan tertinggi Bisa dimanfaatkan untuk memprioritaskan layanan atau penguatan sumber daya berdasarkan wilayah.
+""")
+top_cities = df['City'].value_counts().nlargest(10)
+fig7 = px.bar(x=top_cities.values, y=top_cities.index, orientation='h', labels={'x':'Jumlah Panggilan', 'y':'Kota'}, color=top_cities.index)
+st.plotly_chart(fig7, use_container_width=True)
+
+# 4. Distribusi Sentimen
 st.subheader("💬 Distribusi Sentimen")
 st.markdown("Mengevaluasi persepsi pelanggan terhadap layanan secara keseluruhan.")
 fig3 = px.histogram(filtered_df, x='Sentiment', color='Sentiment', category_orders={'Sentiment': ['Very Negative', 'Negative', 'Neutral', 'Positive', 'Very Positive']})
 st.plotly_chart(fig3, use_container_width=True)
 
-# 4. Rata-rata CSAT Score per Channel
+# 5. Rata-rata CSAT Score per Channel
 st.subheader("📊 Rata-rata CSAT Score per Channel")
 st.markdown("Membandingkan performa layanan antar channel untuk evaluasi dan pengembangan.")
 avg_csat = df.groupby('Channel')['Csat_score'].mean().reset_index()
 fig4 = px.bar(avg_csat, x='Channel', y='Csat_score', color='Channel')
 st.plotly_chart(fig4, use_container_width=True)
 
-# 5. Durasi Panggilan vs CSAT Score
+# 6. Durasi Panggilan vs CSAT Score
 st.subheader("⏱️ Durasi Panggilan berdasarkan CSAT Score")
 st.markdown("Melihat hubungan durasi dan kepuasan pelanggan sebagai evaluasi efisiensi.")
 fig5 = px.box(filtered_df, x='Csat_score', y='Call duration in minutes', points='all', color='Csat_score')
 st.plotly_chart(fig5, use_container_width=True)
 
-# 6. Distribusi Response Time
+# 7. Distribusi Response Time
 st.subheader("⏱️ Distribusi Response Time")
 st.markdown("Menilai kecepatan respon agen dalam menjawab panggilan pelanggan.")
 fig6 = px.histogram(filtered_df, x='Response_time', color='Response_time')
